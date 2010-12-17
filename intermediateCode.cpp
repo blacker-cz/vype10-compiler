3a019435e1dbf44450731c8c02128cd957c09e7a
Lukáš Černý <cerny.l@gmail.com>
2010-12-17 10:08:46 +0100

    Intermediate code queue implementation

--- intermediateCode.cpp
+++ intermediateCode.cpp
@@ -4,13 +4,17 @@
  * Intermediate code class.
  */
 
+#include <iostream>
 #include "intermediateCode.h"
 
 namespace vype10 {
 
+/**
+ * Constructor
+ */
 IntermediateCode::IntermediateCode() {
-	// TODO Auto-generated constructor stub
-
+	// initialization of instruction line iterator
+	instructionLineIter = instructionLine.begin();
 }
 
 IntermediateCode::~IntermediateCode() {
@@ -26,9 +30,63 @@ IntermediateCode::~IntermediateCode() {
  * @param std::string*		pointer with key to the symbol table where to store result
  */
 void IntermediateCode::add(Instruction instruction, std::string* firstParam, std::string* secondParam, std::string* result) {
-	/**
-	 * @todo: implement this :)
-	 */
+	// build instruction record
+	InstructionRecord* ins = new InstructionRecord();
+	ins->instruction = instruction;
+	ins->first = firstParam;
+	ins->second = secondParam;
+	ins->result = result;
+
+	// push instruction record to the end of instruction line
+	instructionLine.push_back(ins);
+
+	// debug output
+	std::cerr << "Instruction: " << instruction << ", " << (firstParam==(std::string*)NULL ? "NULL" : *firstParam)
+			<< ", " << (secondParam==(std::string*)NULL ? "NULL" : *secondParam)
+			<< ", "	<< (result==(std::string*)NULL ? "NULL" : *result) << std::endl;
+}
+
+/**
+ * Gets actual instruction record and moves private iterator to the next one.
+ *
+ * @return InstructionRecord*		pointer to the instruction record or NULL if no instruction is available
+ */
+IntermediateCode::InstructionRecord *IntermediateCode::get(void) {
+	if(instructionLineIter == instructionLine.end())
+		return (InstructionRecord*) NULL;
+
+	// save actual instruction record
+	InstructionRecord *tmp;
+	tmp = *instructionLineIter;
+
+	// move iterator to next instruction symbol
+	instructionLineIter++;
+
+	// return actual instruction symbol
+	return tmp;
+}
+
+/**
+ * Gets next instruction record without moving private iterator.
+ * So if you use peek and next get -> you get same record.
+ *
+ * @return InstructionRecord*		pointer to the instruction record or NULL if no instruction is available
+ */
+IntermediateCode::InstructionRecord *IntermediateCode::peek(void) {
+	if((instructionLineIter+1) == instructionLine.end())
+		return (InstructionRecord*) NULL;
+
+	return *(instructionLineIter+1);
+}
+
+/**
+ * Sets internal iterator to the previous instruction record.
+ */
+void IntermediateCode::rollback(void) {
+	if(instructionLineIter == instructionLine.begin())
+		return;
+
+	--instructionLineIter;
 }
 
 }
--- intermediateCode.h
+++ intermediateCode.h
@@ -17,16 +17,41 @@ public:
 	IntermediateCode();
 	virtual ~IntermediateCode();
 
-	/* Enum with possible instructions */
-	typedef enum Instruction_T {FUNC_CALL, PRINT, STRCAT, READ_CHAR, READ_STRING,
+	/** Enum with possible instructions */
+	typedef enum Instruction_T {FUNC_CALL = 0, PRINT, STRCAT, READ_CHAR, READ_STRING,
 		READ_INT, READ_SHORT, IF, ELSE, WHILE, CONTINUE, BREAK, RETURN, AND, OR,
 		BIN_AND, BIN_OR, EQ, NE, GT, LT, GE, LE, ADD, SUB, MUL, DIV, MOD, NOT,
-		BIN_NOT, INDEX} Instruction;
+		BIN_NOT, INDEX, NOP, ASSIGN, CAST, ENDIF, ENDWHILE, PUSH} Instruction;
+
+	/** Structure for keeping instruction record */
+	typedef struct InstructionRecord_T {
+		Instruction 	instruction;
+		std::string*	first;
+		std::string*	second;
+		std::string*	result;
+
+		InstructionRecord_T() {
+			instruction = NOP;
+			first = (std::string*) NULL;
+			second = (std::string*) NULL;
+			result = (std::string*) NULL;
+		}
+	} InstructionRecord;
 
 	void add(Instruction instruction, std::string* firstParam, std::string* secondParam, std::string* result);
 
-private:
+	InstructionRecord *get(void);
 
+	InstructionRecord *peek(void);
+
+	void rollback(void);
+
+private:
+	/** Vector for keeping instruction line */
+	std::vector<InstructionRecord*> instructionLine;
+
+	/** Iterator for instruction line vector */
+	std::vector<InstructionRecord*>::iterator instructionLineIter;
 };
 
 }
